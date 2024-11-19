a7d1d92c1a4563f8fe8e8daca76ef2bf514dd151
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-11-18 22:39:12 -0800

    trying to work the buttons

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 document.querySelector('#resetTo0').addEventListener('click', runReset)
 
+let total = 1
+function runReset () {
+    total = 0
+    document.querySelector('#resetTo0')
+    document.querySelector('#display').value = total = 0
+}
